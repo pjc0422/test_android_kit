90086d6e731704b08bfc84afd29fb3e92d766d35
박주철 (모빌리티앱개발TF) <park.jc@kt.com>
2023-05-17 11:54:09 +0900

    test init

---
+++ app/src/main/java/com/kt/example/testkitmodule/Vehicle.kt
@@ -0,0 +1,5 @@
+package com.kt.example.testkitmodule
+
+interface Vehicle {
+    fun move()
+}
---
+++ app/src/main/java/com/kt/example/testkitmodule/device/Car.kt
@@ -0,0 +1,10 @@
+package com.kt.example.testkitmodule.device
+
+import com.kt.example.testkitmodule.Vehicle
+
+class Car : Vehicle {
+
+    override fun move() {
+        print("Car Move")
+    }
+}
